ec9df67eacf9be71dbad0da50bbc8ce3877f264a
Simonov Anton <flash_anton@mail.ru>
2023-07-05 09:05:56 +0300

    + шаблон задачи и теста

---
+++ src/test/java/org/example/TestTemplate.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.function.BiConsumer;
+
+@DisplayName("contestNumber-task")
+public class TestTemplate extends ContestChecker {
+    @Override
+    public BiConsumer<InputStream, OutputStream> getTaskAlgorithm() {
+        return (reader, writer) -> {
+            try {
+                Template.Main(reader, writer);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+    @Test
+    void example1() {
+        check("""
+                123
+                """, """
+                123
+                """);
+    }
+
+    @Test
+    void test1() {
+        check("""
+                  
+                """, """
+                                
+                """);
+    }
+}
