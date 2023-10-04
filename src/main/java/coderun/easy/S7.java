7b97b11cb6424a0a2fd2333bebb739843b18d1fe
Simonov Anton <flash_anton@mail.ru>
2023-10-04 15:21:21 +0300

    coderun : + 7. Поиск в глубину

---
+++ src/main/java/coderun/easy/S7.java
@@ -0,0 +1,71 @@
+package coderun.easy;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * <pre>
+ * <a href="https://coderun.yandex.ru/problem/search-in-depth">7. Поиск в глубину</a>
+ * легкая dfs
+ *
+ * Дан неориентированный граф, возможно, с петлями и кратными ребрами. Необходимо построить компоненту связности,
+ * содержащую первую вершину.
+ *
+ * Формат ввода
+ * В первой строке записаны два целых числа N (1≤N≤10^3) и M (0≤M≤5*10^5) — количество вершин и ребер в графе.
+ * В последующих MM строках перечислены ребра — пары чисел, определяющие номера вершин, которые соединяют ребра.
+ *
+ * Формат вывода
+ * В первую строку выходного файла выведите число K — количество вершин в компоненте связности. Во вторую строку
+ * выведите K целых чисел — вершины компоненты связности, перечисленные в порядке возрастания номеров.
+ *
+ * 20  OK  1 с / 2 с  70.7 Мб / 256 Мб
+ * </pre>
+ */
+public class S7 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        alg(reader, writer);
+        reader.close();
+        writer.close();
+    }
+
+    public static void alg(BufferedReader reader, BufferedWriter writer) throws IOException {
+        String[] buf = reader.readLine().split(" ");
+        int N = Integer.parseInt(buf[0]);
+        int M = Integer.parseInt(buf[1]);
+
+        Map<Integer, Set<Integer>> G = new HashMap<>(N, 1);
+        for (int i = 1; i < N + 1; i++) {
+            G.put(i, new HashSet<>());
+        }
+        for (int i = 0; i < M; i++) {
+            buf = reader.readLine().split(" ");
+            int a = Integer.parseInt(buf[0]);
+            int b = Integer.parseInt(buf[1]);
+            if (a != b) {
+                G.get(a).add(b);
+                G.get(b).add(a);
+            }
+        }
+
+        Set<Integer> visited = new HashSet<>();
+        Deque<Integer> unvisited = new ArrayDeque<>(Set.of(1));
+        while (!unvisited.isEmpty()) {
+            int v = unvisited.poll();
+            if (visited.add(v)) {
+                unvisited.addAll(G.get(v));
+            }
+        }
+
+        List<Integer> component = new ArrayList<>(visited);
+        Collections.sort(component);
+
+        StringBuilder sb = new StringBuilder();
+        component.forEach(i -> sb.append(i).append(' '));
+
+        writer.write(component.size() + "\n" + sb);
+        writer.flush();
+    }
+}
---
+++ src/test/java/coderun/easy/S7Test.java
@@ -0,0 +1,39 @@
+
+
+package coderun.easy;
+
+import org.example.ContestChecker;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.function.BiConsumer;
+
+class S7Test extends ContestChecker {
+    private static final BiConsumer<InputStream, OutputStream> algorithm = (reader, writer) -> {
+        try {
+            S7.alg(new BufferedReader(new InputStreamReader(reader)), new BufferedWriter(new OutputStreamWriter(writer)));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    };
+
+    @Override
+    public BiConsumer<InputStream, OutputStream> getTaskAlgorithm() {
+        return algorithm;
+    }
+
+    @Test
+    void example1() {
+        check("""
+                4 5
+                2 2
+                3 4
+                2 3
+                1 3
+                2 4
+                """, """
+                4
+                1 2 3 4
+                """);
+    }
+}
