c835c30182b5bc849dc1a531fcf41fbd7f6b44ff
Simonov Anton <flash_anton@mail.ru>
2023-10-16 14:56:10 +0300

    Тренировка 1.0: занятие 3: + I. Полиглоты

---
+++ src/test/java/contest/algorithms/training1/contest27663/IHTest.java
@@ -0,0 +1,45 @@
+package contest.algorithms.training1.contest27663;
+
+import common.ContestChecker;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.function.BiConsumer;
+
+class IHTest extends ContestChecker {
+    private static final BiConsumer<InputStream, OutputStream> algorithm = (reader, writer) -> {
+        try {
+            I.alg(new BufferedReader(new InputStreamReader(reader)), new BufferedWriter(new OutputStreamWriter(writer)));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    };
+
+    @Override
+    public BiConsumer<InputStream, OutputStream> getTaskAlgorithm() {
+        return algorithm;
+    }
+
+    @Test
+    void example1() {
+        check("""
+                3
+                3
+                Russian
+                English
+                Japanese
+                2
+                Russian
+                English
+                1
+                English
+                """, """
+                1
+                English
+                3
+                Russian
+                Japanese
+                English
+                """);
+    }
+}
