20b4cc1edca381eaa37d192e9b5d3aad7a9ee6af
Simonov Anton <flash_anton@mail.ru>
2023-10-19 19:40:03 +0300

    Тренировка 1.0: занятие 2: + I. Сапер (2 способ)

--- contest/src/main/java/contest/algorithms/training1/contest27472/I.java
+++ contest/src/main/java/contest/algorithms/training1/contest27472/I.java
@@ -44,10 +44,42 @@ public class I {
         writer.close();
     }
 
-    private static void addIfNotMine(int[][] F, int n, int m) {
-        if (F[n][m] >= 0) {
-            F[n][m]++;
+    public static void alg(BufferedReader reader, BufferedWriter writer) throws IOException {
+        int[] buf = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int N = buf[0];
+        int M = buf[1];
+        int K = buf[2];
+        int[][] mines = new int[K][];
+        for (int i = 0; i < K; i++) {
+            mines[i] = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
+
+        String solution = alg1(N, M, K, mines);
+
+        writer.write(solution);
+        writer.flush();
+    }
+
+    private static String toStr(int N, int M, int[][] F) {
+        StringBuilder sb = new StringBuilder();
+        for (int n = 1; n < N + 1; n++) {
+            for (int m = 1; m < M + 1; m++) {
+                sb.append((F[n][m] >= 0) ? String.valueOf(F[n][m]) : "*").append(" ");
+            }
+            sb.setCharAt(sb.length() - 1, '\n');
+        }
+        return sb.toString();
+    }
+
+    public static String alg1(int N, int M, int K, int[][] mines) {
+        int[][] F = new int[1 + N + 1][1 + M + 1];
+        for (int i = 0; i < K; i++) {
+            int n = mines[i][0];
+            int m = mines[i][1];
+
+            addMine(F, n, m);
+        }
+        return toStr(N, M, F);
     }
 
     private static void addMine(int[][] F, int n, int m) {
@@ -62,29 +94,27 @@ public class I {
         addIfNotMine(F, n + 1, m + 1);
     }
 
-    public static void alg(BufferedReader reader, BufferedWriter writer) throws IOException {
-        int[] buf = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int N = buf[0];
-        int M = buf[1];
-        int K = buf[2];
+    private static void addIfNotMine(int[][] F, int n, int m) {
+        if (F[n][m] >= 0) {
+            F[n][m]++;
+        }
+    }
+
+    public static String alg2(int N, int M, int K, int[][] mines) {
+        int[][] neighbourNMOffset = {
+                {1, -1}, {1, 0}, {1, 1},
+                {0, -1}, {0, 1},
+                {-1, -1}, {-1, 0}, {-1, 1}
+        };
 
         int[][] F = new int[1 + N + 1][1 + M + 1];
         for (int i = 0; i < K; i++) {
-            buf = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            int n = buf[0];
-            int m = buf[1];
-            addMine(F, n, m);
-        }
+            int n = mines[i][0];
+            int m = mines[i][1];
 
-        StringBuilder sb = new StringBuilder();
-        for (int n = 1; n < N + 1; n++) {
-            for (int m = 1; m < M + 1; m++) {
-                sb.append((F[n][m] >= 0) ? String.valueOf(F[n][m]) : "*").append(" ");
-            }
-            sb.setCharAt(sb.length()-1, '\n');
+            F[n][m] = -neighbourNMOffset.length - 1;
+            Arrays.stream(neighbourNMOffset).forEach(offsetNM -> F[n + offsetNM[0]][m + offsetNM[1]]++);
         }
-
-        writer.write(sb.toString());
-        writer.flush();
+        return toStr(N, M, F);
     }
 }
--- contest/src/test/java/contest/algorithms/training1/contest27472/ITest.java
+++ contest/src/test/java/contest/algorithms/training1/contest27472/ITest.java
@@ -4,7 +4,12 @@ import common.ContestChecker;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class ITest extends ContestChecker {
     private static final BiConsumer<InputStream, OutputStream> algorithm = (reader, writer) -> {
@@ -57,5 +62,32 @@ class ITest extends ContestChecker {
                 2 2 2 1
                 1 * 2 *
                 """);
+    }
+
+    @Test
+    public void stressTest() {
+        ThreadLocalRandom r = ThreadLocalRandom.current();
+        int N = 6;
+        int M = 6;
+        int K = 5;
+        int[][] mines = new int[K][2];
+        for (int i = 0; i < 100_000; i++) {
+            Map<Integer, Set<Integer>> cache = new HashMap<>();
+            int k = 0;
+            while (k < K) {
+                int n = r.nextInt(1, N + 1);
+                int m = r.nextInt(1, M + 1);
+                if (cache.computeIfAbsent(n, key -> new HashSet<>()).add(m)) {
+                    mines[k][0] = n;
+                    mines[k][1] = m;
+                    k++;
+                }
+            }
+
+            String c1 = I.alg1(N, M, K, mines);
+            String c2 = I.alg2(N, M, K, mines);
+            assertEquals(c1, c2, String.format("\n%s %s %s\n%s\n\nExpected:\n%s\n\nActual:\n%s\n", N, M, K,
+                    Arrays.stream(mines).map(b -> b[0] + " " + b[1]).collect(Collectors.joining("\n")), c1, c2));
+        }
     }
 }
