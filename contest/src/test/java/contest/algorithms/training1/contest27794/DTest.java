69520eeb5a566918fbad3c870c9b1b21d3b55882
Simonov Anton <flash_anton@mail.ru>
2023-10-22 20:05:58 +0300

    Тренировка 1.0: занятие 5: + D. Город Че

---
+++ contest/src/test/java/contest/algorithms/training1/contest27794/DTest.java
@@ -0,0 +1,32 @@
+package contest.algorithms.training1.contest27794;
+
+import common.ContestChecker;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.function.BiConsumer;
+
+class DTest extends ContestChecker {
+    private static final BiConsumer<InputStream, OutputStream> algorithm = (reader, writer) -> {
+        try {
+            D.alg(new BufferedReader(new InputStreamReader(reader)), new BufferedWriter(new OutputStreamWriter(writer)));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    };
+
+    @Override
+    public BiConsumer<InputStream, OutputStream> getTaskAlgorithm() {
+        return algorithm;
+    }
+
+    @Test
+    void example1() {
+        check("""
+                4 4
+                1 3 5 8
+                """, """
+                2
+                """);
+    }
+}
