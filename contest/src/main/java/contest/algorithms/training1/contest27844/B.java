3fa1292832b42f12f5d9eaadfb7486c0d323fc77
Simonov Anton <flash_anton@mail.ru>
2023-10-27 01:36:12 +0300

    Тренировка 1.0: занятие 6: + B. Приближенный двоичный поиск

---
+++ contest/src/main/java/contest/algorithms/training1/contest27844/B.java
@@ -0,0 +1,64 @@
+package contest.algorithms.training1.contest27844;
+
+import java.io.*;
+import java.util.Arrays;
+
+/**
+ * <pre>
+ * <a href="https://contest.yandex.ru/contest/27844">Яндекс. Тренировки по алгоритмам июнь 2021, занятие 6. Бинарный поиск</a>
+ * <a href="https://contest.yandex.ru/contest/27844/run-report/94768062">OK  0.622s  32.01Mb</a>
+ *
+ * B. Приближенный двоичный поиск
+ * Ограничение времени 	1 секунда
+ * Ограничение памяти 	64Mb
+ * Ввод 	стандартный ввод или input.txt
+ * Вывод 	стандартный вывод или output.txt
+ *
+ * Для каждого из чисел второй последовательности найдите ближайшее к нему в первой.
+ *
+ * Формат ввода
+ * В первой строке входных данных содержатся числа N и K (0 < N, K <= 100 001).
+ * Во второй строке задаются N чисел первого массива, отсортированного по неубыванию,
+ * а в третьей строке – K чисел второго массива.
+ * Каждое число в обоих массивах по модулю не превосходит 2*10^9.
+ *
+ * Формат вывода
+ * Для каждого из K чисел выведите в отдельную строку число из первого массива, наиболее близкое к данному.
+ * Если таких несколько, выведите меньшее из них.
+ * </pre>
+ */
+public class B {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        alg(reader, writer);
+        reader.close();
+        writer.close();
+    }
+
+    public static void alg(BufferedReader reader, BufferedWriter writer) throws IOException {
+        reader.readLine();
+        int[] n = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] k = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        StringBuilder sb = new StringBuilder();
+        for (int e : k) {
+            int i = Arrays.binarySearch(n, e);
+            if (i >= 0) {
+                sb.append(n[i]);
+            } else if (i == -1) {
+                sb.append(n[-i - 1]);
+            } else if (i == -n.length - 1) {
+                sb.append(n[-i - 2]);
+            } else if (e - n[-i - 2] <= n[-i - 1] - e) {
+                sb.append(n[-i - 2]);
+            } else {
+                sb.append(n[-i - 1]);
+            }
+            sb.append("\n");
+        }
+
+        writer.write(sb.toString());
+        writer.flush();
+    }
+}
---
+++ contest/src/test/java/contest/algorithms/training1/contest27844/BTest.java
@@ -0,0 +1,78 @@
+package contest.algorithms.training1.contest27844;
+
+import common.ContestChecker;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.function.BiConsumer;
+
+class BTest extends ContestChecker {
+    private static final BiConsumer<InputStream, OutputStream> algorithm = (reader, writer) -> {
+        try {
+            B.alg(new BufferedReader(new InputStreamReader(reader)), new BufferedWriter(new OutputStreamWriter(writer)));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    };
+
+    @Override
+    public BiConsumer<InputStream, OutputStream> getTaskAlgorithm() {
+        return algorithm;
+    }
+
+    @Test
+    void example1() {
+        check("""
+                5 5
+                1 3 5 7 9\s
+                2 4 8 1 6\s
+                """, """
+                1
+                3
+                7
+                1
+                5
+                """);
+    }
+
+    @Test
+    void example2() {
+        check("""
+                6 11
+                1 1 4 4 8 120\s
+                1 2 3 4 5 6 7 8 63 64 65\s
+                """, """
+                1
+                1
+                4
+                4
+                4
+                4
+                8
+                8
+                8
+                8
+                120
+                """);
+    }
+
+    @Test
+    void example3() {
+        check("""
+                10 10
+                -5 1 1 3 5 5 8 12 13 16\s
+                0 3 7 -17 23 11 0 11 15 7\s
+                """, """
+                1
+                3
+                8
+                -5
+                16
+                12
+                1
+                12
+                16
+                8
+                """);
+    }
+}
