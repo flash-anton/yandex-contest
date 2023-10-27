76622d512ebd739eb1a63f180c401e0093b8e0fc
Simonov Anton <flash_anton@mail.ru>
2023-10-27 19:14:35 +0300

    Тренировка 1.0: занятие 6: + D. Космическое поселение

---
+++ contest/src/test/java/contest/algorithms/training1/contest27844/DTest.java
@@ -0,0 +1,58 @@
+package contest.algorithms.training1.contest27844;
+
+import common.ContestChecker;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.function.BiConsumer;
+
+class DTest extends ContestChecker {
+    private static final BiConsumer<InputStream, OutputStream> algorithm = (reader, writer) -> {
+        try {
+            D.alg(new BufferedReader(new InputStreamReader(reader)), new BufferedWriter(new OutputStreamWriter(writer)));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    };
+
+    @Override
+    public BiConsumer<InputStream, OutputStream> getTaskAlgorithm() {
+        return algorithm;
+    }
+
+    @Test
+    void example1() {
+        check("""
+                1 1 1 1 1
+                """, """
+                0
+                """);
+    }
+
+    @Test
+    void example2() {
+        check("""
+                1 1 1 3 3
+                """, """
+                1
+                """);
+    }
+
+    @Test
+    void example3() {
+        check("""
+                11 3 2 21 25
+                """, """
+                2
+                """);
+    }
+
+    @Test
+    void test27() {
+        check("""
+                1000000000 1000000000 1000000000 1000000000000000000 1000000000000000000
+                """, """
+                15810776602472
+                """);
+    }
+}
