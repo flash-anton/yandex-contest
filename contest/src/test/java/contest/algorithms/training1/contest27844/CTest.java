3c4715aac049b2f8a4bd7d3cefefc9fe2522536a
Simonov Anton <flash_anton@mail.ru>
2023-10-27 16:40:14 +0300

    Тренировка 1.0: занятие 6: + C. Дипломы

---
+++ contest/src/test/java/contest/algorithms/training1/contest27844/CTest.java
@@ -0,0 +1,40 @@
+package contest.algorithms.training1.contest27844;
+
+import common.ContestChecker;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.function.BiConsumer;
+
+class CTest extends ContestChecker {
+    private static final BiConsumer<InputStream, OutputStream> algorithm = (reader, writer) -> {
+        try {
+            C.alg(new BufferedReader(new InputStreamReader(reader)), new BufferedWriter(new OutputStreamWriter(writer)));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    };
+
+    @Override
+    public BiConsumer<InputStream, OutputStream> getTaskAlgorithm() {
+        return algorithm;
+    }
+
+    @Test
+    void example1() {
+        check("""
+                2 3 10
+                """, """
+                9
+                """);
+    }
+
+    @Test
+    void test6() {
+        check("""
+                1000000000 1000000000 1000000000
+                """, """
+                9
+                """);
+    }
+}
