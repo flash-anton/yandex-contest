500466b85dbaaba970a7d44d16ec5dfd4de84cf3
Simonov Anton <flash_anton@mail.ru>
2023-11-02 00:51:22 +0300

    Тренировка 4.0: Разминка: + I. Правильная скобочная последовательность

---
+++ contest/src/test/java/contest/algorithms/training4/contest53027/ITest.java
@@ -0,0 +1,71 @@
+package contest.algorithms.training4.contest53027;
+
+import common.ContestChecker;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.io.*;
+import java.util.function.BiConsumer;
+import java.util.stream.Stream;
+
+class ITest extends ContestChecker {
+    private static final BiConsumer<InputStream, OutputStream> algorithm = (reader, writer) -> {
+        try {
+            I.alg(new BufferedReader(new InputStreamReader(reader)), new BufferedWriter(new OutputStreamWriter(writer)));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    };
+
+    @Override
+    public BiConsumer<InputStream, OutputStream> getTaskAlgorithm() {
+        return algorithm;
+    }
+
+    @Test
+    void example1() {
+        check("""
+                ()[]
+                """, """
+                yes
+                """);
+    }
+
+    @Test
+    void example2() {
+        check("""
+                ([)]
+                """, """
+                no
+                """);
+    }
+
+    @Test
+    void example3() {
+        check("""
+                (
+                """, """
+                no
+                """);
+    }
+
+    static class ArgProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) {
+            return Stream.of(
+                    Arguments.of("yes", ""),
+                    Arguments.of("no", ")")
+            );
+        }
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(ArgProvider.class)
+    void test(String expected,String req) {
+        check(req, expected);
+    }
+}
