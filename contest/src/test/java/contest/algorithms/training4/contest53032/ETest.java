da54535d250a407f9101f60c9d8e2fe83f7222f5
Simonov Anton <flash_anton@mail.ru>
2023-11-24 23:03:11 +0300

    Алг 4.0 ДЗ 4 (53032): + E. Генерация правильных скобочных последовательностей - 2

---
+++ contest/src/test/java/contest/algorithms/training4/contest53032/ETest.java
@@ -0,0 +1,54 @@
+package contest.algorithms.training4.contest53032;
+
+import common.ContestChecker;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.time.Duration;
+import java.util.function.BiConsumer;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
+@DisplayName("Алг 4.0 ДЗ 4 (53032) Перебор и методы его оптимизации - E")
+class ETest extends ContestChecker {
+    private static final BiConsumer<InputStream, OutputStream> algorithm = (reader, writer) -> {
+        try {
+            E.alg(new BufferedReader(new InputStreamReader(reader)), new BufferedWriter(new OutputStreamWriter(writer)));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    };
+
+    @Override
+    public BiConsumer<InputStream, OutputStream> getTaskAlgorithm() {
+        return algorithm;
+    }
+
+    @Test
+    void example1() {
+        check("""
+                4
+                """, """
+                (())
+                ([])
+                ()()
+                ()[]
+                [()]
+                [[]]
+                []()
+                [][]
+                """);
+    }
+
+    @Test
+    void testTL() {
+        for (int i = 0; i < 10; i++) {
+            int n = 16;
+            assertAll(
+                    () -> assertTimeout(Duration.ofSeconds(2), () -> E.alg1(n), "alg1() is too slow")
+            );
+        }
+    }
+}
