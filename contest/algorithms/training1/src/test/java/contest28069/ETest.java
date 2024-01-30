35c2376467b7f46cbed5e6d9d43761b64c0b62cb
Simonov Anton <flash_anton@mail.ru>
2024-01-30 22:39:09 +0300

    Алг 1.0 ДЗ 8 (28069): + E. Вывод листьев

---
+++ contest/algorithms/training1/src/test/java/contest28069/ETest.java
@@ -0,0 +1,40 @@
+package contest28069;
+
+import common.ContestChecker;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.function.BiConsumer;
+
+@DisplayName("Алг 1.0 ДЗ 8 (28069) - E")
+class ETest extends ContestChecker {
+    private static final BiConsumer<InputStream, OutputStream> algorithm = (reader, writer) -> {
+        try {
+            E.alg(reader, new BufferedWriter(new OutputStreamWriter(writer)));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    };
+
+    @Override
+    public BiConsumer<InputStream, OutputStream> getTaskAlgorithm() {
+        return algorithm;
+    }
+
+    @Test
+    void example1() {
+        check("""
+                7 3 2 1 9 5 4 6 8 0
+                """, """
+                1
+                4
+                6
+                8
+                                
+                """);
+    }
+}
